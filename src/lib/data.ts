beb32f9228262e87fd6a31bb189ad5d4ba2cf53e
DiegoHormazabalGIT <dhormazabal.vc@gmail.com>
2025-05-31 00:53:25 -0400

    portfolio version 1

---
+++ src/lib/data.ts
@@ -0,0 +1,101 @@
+export const personalInfo = {
+  name: "Diego H",
+  location: "Los Andes, Chile",
+  email: "dhormazabal.vc@gmail.com",
+  github: "https://github.com/DiegoHormazabalGIT/DiegoHormazabal",
+  linkedin: "https://www.linkedin.com/in/dhormazabal",
+};
+
+export const workExperience = [
+  {
+    company: "Rayen Salud",
+    location: "Remote",
+    position: "Analista QA",
+    period: "Feb 2022 - Present",
+    achievements: [
+      "Automatización de pruebas de regresión críticas utilizando Selenium (Python).",
+      "Desarrollo y mantenimiento de un framework de automatización basado en el patrón Page Object Model (POM), mejorando la escalabilidad y mantenibilidad del código de pruebas.",
+      "Ejecución de pruebas funcionales, smoke y exploratorias sobre sistemas clínicos y de gestión médica",
+      "Participación activa en ceremonias ágiles (daily, planning, retrospectivas) dentro de un equipo SCRUM, contribuyendo a la mejora continua de los procesos de QA.",
+      "Diseño y ejecución de casos de prueba funcionales, regresión y smoke, basados en historias de usuario y criterios de aceptación.",
+      "Realización de pruebas exploratorias para detectar bugs no cubiertos por pruebas automatizadas, aportando valor desde una perspectiva de usuario final.",
+      
+    ],
+  },
+  
+];
+
+export const education = [
+  {
+    institution: "Instituto Profesional AIEP",
+    location: "San Felipe, Chile",
+    degree: "Tecnico En Programacion Y Analisis De Sistemas",
+    period: "2017 - 2020",
+    achievements: [
+      
+    ],
+  },
+
+  {
+    institution: "Instituto Profesional AIEP",
+    location: "San Felipe, Chile",
+    degree: "Ingenieria Informatica Mencion Desarrollo De Sistemas",
+    period: "2020 - 2021",
+    
+  },
+
+  {
+    institution: "Test -Group",
+    location: "Santiago, Chile",
+    degree: "ISTQB - CFL",
+    period: "Abril - 2025",
+    
+  },
+];
+export const skills = {
+  programmingLanguages: [
+    "Smoke Test",
+    "E2E Testing",
+    "Automation Testing"
+  ],
+  frontendDevelopment: [
+    "Selenium WebDriver",
+    "Python",
+    "Pytest",
+    "Cypress",
+    "POM",
+    "JavaScript",
+  ],
+  backendDevelopment: ["Nodejs", "Expressjs"],
+  databaseAndStorage: ["PostgreSQL", "Drizzle (ORM)"],
+  cloudAndDevOps: ["Azure DevOps", "Azure Pipelines", "CI/CD"],
+  toolsAndServices: [
+    "Clerk (Auth)",
+    "Sanity (CMS)",
+    "Tinybird (analytics)",
+    "Zod",
+    "Sentry",
+    "Mixpanel",
+    "Trigger.dev",
+  ],
+};
+
+export const projects = [
+  {
+    title: "Proyecto 1",
+    github: "https://github.com/rishikesh2003/Prodigi",
+    description: [
+      "",
+    ],
+  },
+  {
+    title: "Proyecto 2",
+    github: "https://github.com/rishikesh2003/mental-aarog",
+    description: [
+      "",
+    ],
+  },
+];
+
+
+
